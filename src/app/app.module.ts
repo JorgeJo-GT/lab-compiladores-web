fa89643dc4878d7dcef636382a64127924b8bad2
Jorge Jó <jalvarado@gyt.com.gt>
2022-04-08 19:40:56 -0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { InputNumberModule } from "primeng/inputnumber";
+import {StyleClassModule} from 'primeng/styleclass';
+
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    InputNumberModule,
+    StyleClassModule
   ],
   providers: [],
   bootstrap: [AppComponent]
